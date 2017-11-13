ac383036e45cd0b789adccd447e14bf17c5af95f
Daniel Morris <daniellmorris@gmail.com>
2017-11-13 16:49:55 -0600

    Cleanup content script code

--- content/content.js
+++ content/content.js
@@ -12,9 +12,7 @@ function actionEls(els, unhide) {
   var newCount = els.length;
   if (els.length) {
     for (el of els) {
-      //console.log(jq, hide)
       if (hide.debug) {
-        //console.log("Debugging");
         el.style['background-color'] = 'aqua'
         el.classList.add('hide-elements-now');
       } else {
@@ -31,33 +29,6 @@ function actionEls(els, unhide) {
   }
 }
 
-//function action(jq, unhide) {
-//  var newCount = jq.length;
-//  if (jq.length) {
-//    //console.log(jq, hide)
-//    if (hide.debug) {
-//      //console.log("Debugging");
-//      jq.css('background-color', 'aqua')
-//    } else {
-//      //console.log('hide')
-//      //if(unhide) {
-//      //  //hide.allTimeCount = hide.allTimeCount-hide.count+newCount;
-//      //  //hide.count = 0;
-//      //  jq.show()
-//      //} else {
-//        //jq.remove()
-//      //jq.find('.content').hide();
-//      jq.hide();
-//      jq.addClass('hide-elements-now');
-//      //}
-//    }
-//  } 
-//  if (!unhide) {
-//    hide.allTimeCount += newCount;
-//    hide.count += newCount;
-//    chrome.storage.local.set({allTimeCount: hide.allTimeCount, count: hide.count});
-//  }
-//}
 function genDolRegex() {
   var reg = '\\$'
   var i;
@@ -66,26 +37,6 @@ function genDolRegex() {
   }
   return new RegExp(reg, 'gm');
 }
-
-//function filterElsJquery(dolReg, limitTextSearch) {
-//  return function () {
-//      var ret = false;
-//      var text = null;
-//      if (limitTextSearch) {
-//        text = $(this).find(limitTextSearch).text();
-//      } else {
-//        text = $(this).text();
-//      }
-//      if (hide.cash && dolReg.test(text))
-//        ret = true ; 
-//
-//      if (hide.telegram && /t\.me/gi.test(text))
-//        ret = true
-//
-//      //if (ret) console.log("Not Filtered", ret, text.substring(0, 250))
-//      return ret;
-//    }
-//}
 
 function filterEls(dolReg, limitTextSearch) {
   return function (el) {
@@ -102,52 +53,18 @@ function filterEls(dolReg, limitTextSearch) {
 
       if (hide.telegram && /t\.me/gi.test(text))
         ret = true
-
-      //console.log(ret, /t\.me/gi.test(text), dolReg.test(text), text);
-      //if (ret) {
-      //  el.style.display = 'none';
-      //  el.classList.add('hide-elements-now');
-      //}
-      //console.log("Not Filtered", ret, text, el)
       return ret;
     }
 }
 function doHidesRaw() {
   if (hide.init) {
-    //console.log("DoHidesRaw-Start");
-    //var s = new Date()
-    //console.log("doHidesRaw");
     var dolReg = genDolRegex();
-    //console.log(dolReg);
-    //action($('.js-stream-item:contains(t.me)'))
     
     let els = document.querySelectorAll(".js-stream-item:not(.hide-elements-now)");
     els = _.filter(els, filterEls(dolReg, '.js-tweet-text'));
     actionEls(els);   
-    //action($(".js-stream-item:not(.hide-elements-now)").filter(filterElsJquery(dolReg, '.js-tweet-text')), false)
-    //console.log("DoHidesRaw-End", (new Date()).getTime() - s.getTime());
   }
 }
-
-//function doShows() {
-//  if (hide.init) {
-//    var dolReg = genDolRegex();
-//    //console.log(dolReg);
-//      //action($('.js-stream-item:contains(t.me)'))
-//    action($(".js-stream-item").filter(function () {
-//      var ret = false;
-//      var text = $(this).find('.js-tweet-text').text();
-//      if (!hide.cash && dolReg.test(text))
-//        ret = true ; 
-//
-//      if (!hide.telegram && /t\.me/gi.test(text))
-//        ret = true
-//
-//      return ret;
-//    }), true)
-//  }
-//}
-
 //var doHides = doHidesRaw;//_.throttle(doHidesRaw, 100);
 var doHides = _.throttle(doHidesRaw, 100);
 
@@ -161,7 +78,6 @@ var doHideInit = _.throttle(function() {
     $(window).scroll(function() {
       if (hide.cash || hide.telegram) {
         if((Math.ceil($(window).scrollTop()) + $(window).height()) >= ($(document).height())) {
-          //console.log("Scrolled to bottom");
           forceReload();
         }
       }
@@ -176,40 +92,7 @@ $(document).ready(function() {
   setTimeout(doHideInit(), 0);
 
   var observer = new MutationObserver(function(mutations) {
-    //mutations.forEach(function(m) {
-    //  if (m.addedNodes && m.addedNodes.length>0) {
-    //    var dolReg = genDolRegex();
-    //    console.log('observe2', m.addedNodes[0], $(m.addedNodes).find('.js-stream-item').length, m.type, $(m.addedNodes).filter(filterEls(dolReg)));
-    //    //action($(".js-stream-item:not(.hide-elements-now)").filter(filterEls(dolReg)), false)
-    //    //m.addedNodes.forEach(function() {
-
-    //    //})
-    //  }
-    //});
-    //let shouldDoHides = false;
-    //mutations.forEach(function(mutation) {
-    //    //console.log(mutation)
-    //    if (mutation.addedNodes && mutation.addedNodes.length > 0) {
-    //        // element added to DOM
-    //        var hasClass = [].some.call(mutation.addedNodes, function(el) {
-    //            return el.classList?el.classList.contains('js-stream-item'):false
-    //        });
-    //        if (hasClass) {
-    //          shouldDoHides = true;
-    //            // element has class `MyClass`
-    //            console.log('element ".js-stream-item" added');
-    //        }
-    //    }
-    //});
-    
-    //var dolReg = genDolRegex();
-    //console.log("Test", $(_.pluck(mutations, 'target')), $(_.pluck(mutations, 'target')).filter(filterEls(dolReg)));
-    //if (shouldDoHides) {
-      doHides();
-      //doHides();
-    //} else {
-    //  console.log(mutations);
-    //}
+    doHides();
   });
 
   observer.observe(document, {
@@ -259,7 +142,6 @@ $(document).ready(function() {
         || changes.cashNum && changes.cashNum.newValue!=changes.cashNum.oldValue
         || changes.telegram && changes.telegram.newValue!=changes.telegram.oldValue
         ) {
-      //doShows();
       location.reload();
       doHides();
     }
